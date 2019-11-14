c44bf19888042ec8d5a9822a911a2a7a5342f461
tina920921 <1980294900@qq.com>
2019-11-14 11:56:50 +0800

    add index

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ##Tina 社区
 
 ##资料
+spring 外面文档
+bootstrap 文档
+github app 文档
 
 ##工具^
---
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,11 @@
+package com.majiang.community.controller;
+
+/**
+ * @program: community
+ * @description
+ * @author: tina.liu
+ * @create: 2019-11-14 11:54
+ **/
+public class AuthorizeController {
+
+}
