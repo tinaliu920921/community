12817fdbd252f36ec2f4f58fe02f559d42a628cc
tina920921 <1980294900@qq.com>
2019-11-14 14:14:09 +0800

    add indexController

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -1,4 +1,7 @@
 package com.majiang.community.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @program: community
@@ -6,6 +9,22 @@ package com.majiang.community.controller;
  * @author: tina.liu
  * @create: 2019-11-14 11:54
  **/
+@Controller
 public class AuthorizeController {
 
+    @GetMapping(value = "/callback")
+    public String callback(@RequestParam(name="cod") String code,
+                           @RequestParam(name="state") String state){
+        return "index";
+    }
+
 }
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,15 @@
+package com.majiang.community.provider;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @program: community
+ * @description
+ * @author: tina.liu
+ * @create: 2019-11-14 14:12
+ **/
+@Component
+public class GithubProvider {
+
+
+}
