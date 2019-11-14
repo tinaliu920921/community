924ff933a90ca5f53c3af5afa3c654b30e134ed8
tina920921 <1980294900@qq.com>
2019-11-14 11:08:56 +0800

    add index and bootstrap

--- src/main/java/com/majiang/community/controller/IndexController.java
+++ src/main/java/com/majiang/community/controller/IndexController.java
@@ -1,5 +1,4 @@
 package com.majiang.community.controller;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @create: 2019-11-14 09:25
  **/
 @Controller
-public class HelloController {
+public class IndexController {
 
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String hello(String name){
+       return "index";
     }
 
 
