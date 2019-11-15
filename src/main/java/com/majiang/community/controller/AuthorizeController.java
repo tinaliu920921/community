316c0b479fccf6ffbd1a89fbd6be5fc7a14c0678
tina920921 <1980294900@qq.com>
2019-11-15 08:56:29 +0800

    update

--- src/main/java/com/majiang/community/controller/AuthorizeController.java
+++ src/main/java/com/majiang/community/controller/AuthorizeController.java
@@ -4,9 +4,13 @@ import com.majiang.community.dto.GithubUser;
 import com.majiang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @program: community
@@ -32,7 +36,8 @@ public class AuthorizeController {
 
     @GetMapping(value = "/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
@@ -44,10 +49,20 @@ public class AuthorizeController {
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println("user的值："+user);
-        return "index";
-    }
+        if(user!=null){
+            //登陆成功，写cookie 和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            //登陆失败，重新登陆
+            return "redirect:/";
+        }
+    }//方法的大括号
 
-}
+
+
+
+}//类的大括号
 
 
 
